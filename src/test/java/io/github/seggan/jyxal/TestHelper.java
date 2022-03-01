fff9f5089a1578ae24f2c9243d208f06dea38974
Seggan <segganew@gmail.com>
2022-02-28 20:49:56 -0500

    Fix FS exception

--- src/test/java/io/github/seggan/jyxal/TestHelper.java
+++ src/test/java/io/github/seggan/jyxal/TestHelper.java
@@ -1,6 +1,9 @@
 package io.github.seggan.jyxal;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.regex.Pattern;
@@ -14,6 +17,18 @@ public class TestHelper {
     public static void run(String... args) throws IOException {
         String fileName = Path.of(args[0]).getFileName().toString();
         Main.doMain(args, true);
-        Files.delete(Path.of(args[0]).resolveSibling(p.matcher(fileName).replaceAll(".jar")));
+        Path jar = Path.of(args[0]).resolveSibling(p.matcher(fileName).replaceAll(".jar"));
+        URLClassLoader cl = new URLClassLoader(
+                new URL[] { jar.toUri().toURL() },
+                TestHelper.class.getClassLoader()
+        );
+        try {
+            Class<?> clazz = cl.loadClass("jyxal.Main");
+            clazz.getMethod("main", String[].class).invoke(null, (Object) new String[0]);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+        cl.close();
+        Files.delete(jar);
     }
 }
